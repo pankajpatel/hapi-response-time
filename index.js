69bb8512e3d57f232b154adfa1964a96559f15ab
Pankaj Patel <patel.pankaj@live.in>
2016-11-06 11:43:13 +0530

    finalized lib

--- index.js
+++ index.js
@@ -10,15 +10,19 @@ Plugin.register = function(server, options, next) {
         mapUri: function (request, callback) {
 
           var headers = {
-            'X-Req-Start': (new Date()).getTime()
+            'x-req-start': (new Date()).getTime()
           };
 
           var uri = server.info.uri + request.url.path;
           callback(null, uri, headers);
         },
         onResponse: function (err, res, request, reply) {
-
-          reply(res).header('X-Res-End', (new Date()).getTime()).header('X-Res-End', (new Date()).getTime());
+          var start = request.header('x-req-start');
+          var end = (new Date()).getTime();
+          reply(res)
+            .header('x-req-start', start)
+            .header('x-res-end', end);
+            .header('x-response-time', end - start);
         }
       }
     }
