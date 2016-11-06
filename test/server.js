65e4f353457c427b219ed7e4601ce8fdf3376c4e
Pankaj Patel <patel.pankaj@live.in>
2016-11-06 23:41:32 +0530

    more tests

--- test/server.js
+++ test/server.js
@@ -6,12 +6,21 @@ server.register({
   register: require('../index')
 });
 
-server.route({
+server.route([{
   method: 'GET',        // define the method this route will handle
   path: '/john', // this is how you capture route parameters in Hapi
   handler: function(req, reply) { // request handler method
     reply('Hello John!'); // reply with text.
   }
-});
+},{
+  method: 'GET',
+  path: '/timeout',
+  handler: function (request, reply) {
+    setTimeout(function() {
+      reply('Response after 1 second')
+    }, 1000);
+  }
+}]
+);
 
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var server = require("./server");
 
 lab.experiment("Header Tests", function() {
   // tests
-  lab.test("GET /john shou have time Headers", function(done) {
+  lab.test("GET /john should have time Headers", function(done) {
     var options = {
       method: "GET",
       url: "/john"
@@ -17,6 +17,22 @@ lab.experiment("Header Tests", function() {
       Code.expect(response.headers['x-response-time']).to.be.a.number();  //  Expect header x-response-time to be a number
       Code.expect(response.result).to.have.length(11); // Expect result to be "Hello John!" (11 chars long)
       server.stop(done);  // done() callback is required to end the test.
+    });
+  });
+
+  lab.test("GET /timeout should have responsi time more than 1 sec", function(done) {
+    var options = {
+      method: "GET",
+      url: "/timeout"
+    };
+
+    server.inject(options, function(response) {
+      Code.expect(response.headers['x-req-start']).to.be.a.number();
+      Code.expect(response.headers['x-res-end']).to.be.a.number();
+      Code.expect(response.headers['x-response-time']).to.be.a.number();
+      Code.expect(response.headers['x-response-time']).to.be.at.least(1000);
+      Code.expect(response.result).to.have.length(23);
+      server.stop(done);
     });
   });
 });
